8452085e15267f9955619a15e4880cf0b22bbf32
PHCCorso <pedrohccorso@gmail.com>
2019-12-07 19:14:18 -0300

    reset

--- src/constants/room.ts
+++
@@ -1,4 +0,0 @@
-export const ROOM_WIDTH = 50;
-export const ROOM_HEIGHT = 50;
-
-export const MAX_WALL_HITS = 15000;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import handleCreeps from './creep/handler';
-import handleSources from './source/handler';
-import handleSpawns from './spawn/handler';
-import handleTowers from './tower/handler';
-import handleRooms from './room/handler';
-
-module.exports.loop = function() {
-    handleRooms();
-    handleSources();
-    handleCreeps();
-    handleSpawns();
-    handleTowers();
-};
+module.exports.loop = function() {};
